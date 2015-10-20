f68901fe832e3c697351776ce5dfaa1dba1253b3
Sam Serrels <doogly1@gmail.com>
2015-10-20 20:34:14 +0100

    showing admin status on front page

--- index.php
+++ index.php
@@ -1,18 +1,22 @@
 <?php
 	include('preContent.php');
-
 	require("common.php");
 
 	if(empty($_SESSION['user']))  {
 		echo "You are not logged in! <a href=\"logIn.php\">Log in</a>";
 	} else {
 		echo "Welcome back " . $_SESSION['user']['username'];
+		if(!empty($_SESSION['user']['permissionLevel']) && $_SESSION['user']['permissionLevel'] >0)  {
+			echo "<br><b>Adminstrator Status</b><br>";
+		}
+		echo "<br><a href=\"logOut.php\">Log out</a><br><br>";
 	}
 ?>
 
 <p>
 <a href="courses.php">View Courses</a>
 </p>
+
 
 <?php
 	include('afterContent.php');
