3eb44138345ef5dcba22a1a7b3c8eeece294ac8b
Sam Serrels <doogly1@gmail.com>
2015-10-25 01:10:53 +0100

    Home page stuff, social media in the footer.

--- index.php
+++ index.php
@@ -2,15 +2,83 @@
   $activePage = "home";
 	include('preContent.php');
 	require("common.php");
-
-	if(empty($_SESSION['user']))  {
-		echo "You are not logged in! <a href=\"logIn.php\">Log in</a>";
-	} else {
-		include('userHome.php');
-	}
 ?>
 
 
+<div class="col-md-6">
+  <?php
+    if(empty($_SESSION['user']))  {
+      echo "You are not logged in! <a href=\"logIn.php\">Log in</a>";
+    } else {
+      include('userHome.php');
+    }
+  ?>
+  <h4>About us</h4>
+  <p>
+    Napier Management Training ia a company who specialises in delivering world class management training programmes. 
+    Currently we deliver courses in 5 areas: PRINCE2, ITIL, Change Management, Programme Management and Management of Risk. 
+    We deliver each of these courses once a month in one of three locations (Aberdeen, Edinburgh and Glasgow) over 10 months of the year. 
+  </p>
+  <h4>Training Methodology</h4>
+  <p>
+    Each of our courses are taught over a 3 or 5 day (depends on individual course) in an class room environment. 
+    Our courses are taught by the top experts in management training and fully prepare you for the world of project management. 
+    At each of our courses you will be provided with a comprehensive manual to aid you in your training.
+  </p>
+  <p>
+    Our courses are normally assessed by a final exam at the end of your training. 
+    For further information on course assessment please use the menu bar to find out more about individual courses. 
+  </p>
+</div>
+<div class="col-md-6">
+    <div class="notice-board">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                News Feed
+            </div>
+            <div class="panel-body" style="max-height: 300px;  overflow: scroll;">
+                <ul>
+                    <li>
+                        <a href="#">
+                            <span class="glyphicon glyphicon-align-left text-success"> 25/10/15</span>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="glyphicon glyphicon-info-sign text-danger"> 15/10/15</span>
+                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="glyphicon glyphicon-comment text-warning"> 02/10/15</span>
+                            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="glyphicon glyphicon-edit text-danger"> 28/09/15</span>
+                            Sunt in culpa qui officia deserunt mollit anim id est laborum
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="glyphicon glyphicon-edit text-warning"> 01/09/15</span>
+                            Lorem ipsum dolor sit amet ipsum dolor sit amet
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="glyphicon glyphicon-edit text-success"> 20/08/15</span>
+                            Lorem ipsum dolor sit amet ipsum dolor sit amet
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>         
 <?php
 	include('postContent.php');
 ?>
--- postContent.php
+++ postContent.php
@@ -1,20 +1,28 @@
 <?php 
     //this executes at the bottom of every page
 ?>
-          </div></div>
- <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    Napier Management Training &copy; 2015 
-                </div>
-
+</div>
+</div>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                Napier Management Training &copy; 2015
+            </div>
+            <div class="col-md-6">
+                <a href="#" class="btn btn-social btn-facebook">
+                    <i class="fa fa-facebook"></i>&nbsp; Facebook</a>
+                <a href="#" class="btn btn-social btn-google">
+                    <i class="fa fa-google-plus"></i>&nbsp; Google</a>
+                <a href="#" class="btn btn-social btn-twitter">
+                    <i class="fa fa-twitter"></i>&nbsp; Twitter </a>
+                <a href="#" class="btn btn-social btn-linkedin">
+                    <i class="fa fa-linkedin"></i>&nbsp; Linkedin </a>
             </div>
         </div>
-    </footer>
-    <!-- FOOTER SECTION END-->
-    <!-- JAVASCRIPT AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
-    <!-- CORE JQUERY SCRIPTS -->
-    <script src="js/jquery-1.11.1.js"></script>
+    </div>
+</footer>
+<!-- CORE JQUERY SCRIPTS -->
+<script src="js/jquery-1.11.1.js"></script>
 </body>
 </html>
