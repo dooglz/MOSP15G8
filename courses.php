e896d67514ef65ec8ac02d2a2a0a97f5f064b5ff
Finlay Smith <Finlay.Smith@nucleusfinancial.com>
2015-10-28 22:09:31 +0000

    Adding price to courses, adding link to offline pdf

--- courses.php
+++ courses.php
@@ -7,7 +7,7 @@
     die();
   }
   
-  $query = "SELECT c.id, c.title, c.maxEnrolled, c.start, c.end, l.name AS locName, l.id AS locId
+  $query = "SELECT c.id, c.title, c.price, c.maxEnrolled, c.start, c.end, l.name AS locName, l.id AS locId
   FROM courses as c 
   INNER JOIN locations AS l ON c.location = l.id
   WHERE c.status = 1"; 
@@ -27,7 +27,8 @@
     <tr> 
         <th>Course ID</th> 
         <th>Title</th> 
-        <th>Location</th> 
+        <th>Location</th>
+        <th>Price</th> 
         <th>Start</th> 
         <th>End</th>
     </tr> 
@@ -36,12 +37,16 @@
             <td><?php echo $row['id']; ?></td> 
             <td><a href="viewCourse.php?course=<?php echo $row['id']; ?>"><?php echo htmlentities($row['title'], ENT_QUOTES, 'UTF-8'); ?></a></td>
             <td><a href="viewLocation.php?loc=<?php echo $row['locId']; ?>"><?php echo htmlentities($row['locName'], ENT_QUOTES, 'UTF-8'); ?></a></td>
+            <td>£<?php echo $row['price']; ?></td>
             <td><?php echo htmlentities(date( 'd/m/Y H:i', strtotime($row['start'])), ENT_QUOTES, 'UTF-8'); ?></td>
             <td><?php echo htmlentities(date( 'd/m/Y H:i', strtotime($row['end'])), ENT_QUOTES, 'UTF-8'); ?></td>
         </tr> 
     <?php endforeach; ?> 
 </table> 
 
+
+<br /> <br />
+<b>You can sign up for courses offline by sending <a href="offline-pdf.pdf">this application form</a> to the enclosed address, with a cheque written out to "Napier Management Training".</b>
 <?php
     	include('postContent.php');
 ?>
