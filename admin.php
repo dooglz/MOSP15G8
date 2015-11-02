260ee6b9427acdf4b791bb4a282b3689621bb1d4
Finlay Smith <Finlay.Smith@nucleusfinancial.com>
2015-11-02 20:22:17 +0000

    typo

--- admin.php
+++ admin.php
@@ -63,7 +63,7 @@
                 <a href="users.php">Manage Users</a> <br />
                 <a href="courseManagement.php">Manage Courses</a><br />
                 <a href="locationManagement.php">Manage Locations</a><br />
-                <a href="newsManagement.php">Manage News / Offer</a>
+                <a href="newsManagement.php">Manage News / Offers</a>
             </div>
         </div>
     </div>
