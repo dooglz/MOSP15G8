7ca823970c00064f4dfd07187e6039a8127936bd
Finlay Smith <Finlay.Smith@nucleusfinancial.com>
2015-11-03 12:01:31 +0000

    Adding enrollment to all courses list

--- courses.php
+++ courses.php
@@ -7,10 +7,13 @@
     die();
   }
   
-  $query = "SELECT c.id, c.title, c.price, c.maxEnrolled, c.start, c.end, l.name AS locName, l.id AS locId
-  FROM courses as c 
-  INNER JOIN locations AS l ON c.location = l.id
-  WHERE c.status = 1 and c.start >= NOW()"; 
+  $query = "SELECT c.id, c.title, c.price, c.start, c.end, count(ce.course_id) as enrolled, c.maxEnrolled, l.id as locId, l.name as locName 
+    FROM courses as c 
+    INNER JOIN locations AS l ON c.location = l.id
+    LEFT JOIN  course_enrollment as ce ON ce.course_id = c.id
+    WHERE c.status = 1 and c.start >= NOW()
+    GROUP BY c.id
+    ORDER BY c.start ASC"; 
   
   try { 
     $stmt = $db->prepare($query); 
@@ -28,6 +31,7 @@
         <th>Course ID</th> 
         <th>Title</th> 
         <th>Location</th>
+        <th>Enrolled</th>
         <th>Price</th> 
         <th>Start</th> 
         <th>End</th>
@@ -37,6 +41,7 @@
             <td><?php echo $row['id']; ?></td> 
             <td><a href="viewCourse.php?course=<?php echo $row['id']; ?>"><?php echo htmlentities($row['title'], ENT_QUOTES, 'UTF-8'); ?></a></td>
             <td><a href="viewLocation.php?loc=<?php echo $row['locId']; ?>"><?php echo htmlentities($row['locName'], ENT_QUOTES, 'UTF-8'); ?></a></td>
+            <td><?php echo $row['enrolled']; ?>/<?php echo $row['maxEnrolled']; ?></td>
             <td>£<?php echo $row['price']; ?></td>
             <td><?php echo htmlentities(date( 'd/m/Y H:i', strtotime($row['start'])), ENT_QUOTES, 'UTF-8'); ?></td>
             <td><?php echo htmlentities(date( 'd/m/Y H:i', strtotime($row['end'])), ENT_QUOTES, 'UTF-8'); ?></td>
